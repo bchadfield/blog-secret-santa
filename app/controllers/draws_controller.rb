ef2c21a1b979c890b9617fbfd397da61d825d3fd
Ben Chadfield <Ben@Ben.local>
2013-11-03 14:28:21 +1100

    Twitter log in and email collect

---
+++ app/controllers/draws_controller.rb
@@ -0,0 +1,21 @@
+class DrawsController < ApplicationController
+  skip_before_action :authenticate, only: :show
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,20 @@
-def create
-  auth = request.env["omniauth.auth"]
-  user = User.find_by(provider: auth["provider"], uid: auth["uid"]) || User.create_with_omniauth(auth)
-  session[:user_id] = user.id
-  redirect_to root_path
-end
+class SessionsController < ApplicationController
+	skip_before_action :authenticate, only: [:new, :create]
+	def new
 
-def destroy
-  session[:user_id] = nil
-  redirect_to root_path, notice: "Logged out!"
+	end
+
+	def create
+	  auth = request.env["omniauth.auth"]
+	  @user = User.find_by(provider: auth["provider"], uid: auth["uid"]) || User.create_with_omniauth(auth)
+	  session[:user_id] = @user.id
+	  if @user.email
+	  	redirect_to root_path
+	  end
+	end
+
+	def destroy
+	  session[:user_id] = nil
+	  redirect_to root_path, notice: "Logged out!"
+	end
 end
