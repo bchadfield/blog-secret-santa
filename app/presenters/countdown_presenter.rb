fd446354cd8edb9879dc791a51c5271d0909994f
Ben Chadfield <Ben@Ben.local>
2013-11-14 22:38:30 +1100

    Whenever gem added and countdown context

---
+++ app/presenters/countdown_presenter.rb
@@ -0,0 +1,35 @@
+class CountdownPresenter < SimpleDelegator
+  attr_reader :draw
+
+  def initialize(draw, view)
+    super(view)
+    @draw = draw
+  end
+
+  def render_countdown
+  	render_counter << content_tag(:p, until_text, id: "until-text")
+  end
+
+  private
+
+  	def render_counter
+  		if @draw.draw_time.future?
+  			content_tag(:div, nil, id: "countdown", data: { until: "#{@draw.draw_time}" })
+  		else
+  			if @draw.status == "closed"
+  				content_tag(:div, "Sign up now for the next draw", id: "countdown")
+  			else
+  				content_tag(:div, "Drawing now...", id: "countdown")
+  			end
+  		end
+  	end
+
+  	def until_text
+  		case @draw.status
+  		when "open"
+  			"Until secret santas are drawn"
+  		when "matched"
+  			"Until blog gifts are given. Add your gift now."
+  		end
+  	end
+end
