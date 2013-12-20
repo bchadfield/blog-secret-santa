f7429ac8d765ee50a91b98390adae364b7966b8d
Ben Chadfield <benchadfield@gmail.com>
2013-12-21 10:11:50 +1100

    Ensure content has body before gifting

--- app/models/draw.rb
+++ app/models/draw.rb
@@ -53,7 +53,7 @@ class Draw < ActiveRecord::Base
 		matches.each do |match|
 			receiver = User.find_by(id: match.receiver_id)
 			content = Content.find_by(user_id: match.giver_id, draw_id: self.id, status: nil)
-			if receiver && content
+			if receiver && content && content.body
 				content.update(user_id: receiver.id, status: "given")
 				UserMailer.send_gift(receiver, content, self).deliver
 			end
