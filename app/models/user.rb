fc06c95e6b1eb327113efaf84d80e0642d3f0091
Ben Chadfield <benchadfield@gmail.com>
2014-11-18 12:05:51 +1100

    Prepend http to blog urls if scheme is missing

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,14 @@ class User < ActiveRecord::Base
 
 	enum role: { blogger: 0, elf: 1, santa: 2 }
 
+	def blog=(value)
+		unless value.index(/https?:\/\//)
+			value = "http://#{value}"
+		end
+
+		write_attribute(:blog, value)
+	end
+
 	def self.from_omniauth(auth)
 	  unscoped.where(provider: auth["provider"], uid: auth["uid"]).first_or_create do |user|
 	    user.provider = auth["provider"]
