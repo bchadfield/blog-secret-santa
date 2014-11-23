0c608126f3c8f059becf5c271b857b4fab00fcad
Ben Chadfield <benchadfield@gmail.com>
2014-11-23 17:13:19 +1100

    Force prod to ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Secretsanta::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -66,7 +66,7 @@ Secretsanta::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.asset_host = "http://blogsecretsanta.com"
+  config.action_mailer.asset_host = "https://blogsecretsanta.com"
   config.action_mailer.default_url_options = { host: 'blogsecretsanta.com' }
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = { 
