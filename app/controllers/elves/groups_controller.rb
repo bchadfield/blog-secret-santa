fb32ae0ad3fbda1b21794bd184a3dabd3cd90fa4
Ben Chadfield <benchadfield@gmail.com>
2014-12-26 09:19:32 +1100

    N+1 fix for users display in gifted elf/santa area

--- app/controllers/elves/groups_controller.rb
+++ app/controllers/elves/groups_controller.rb
@@ -3,9 +3,9 @@ class Elves::GroupsController < Elves::ElvesController
 	def show
 		@group = current_tenant
 		if params[:filter] && User.valid_scope?(params[:filter])
-			@users = User.send(params[:filter]).includes(:giver).order(:name)
+			@users = User.send(params[:filter]).includes(:giver, receiver_match: :content).order(:name)
 		else
-			@users = User.includes(:giver).order(:name)
+			@users = User.includes(:giver, receiver_match: :content).order(:name)
 		end
 	end
 end
